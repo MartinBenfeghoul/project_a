04093f536991f01c2a4208d150e6d26c7a8c0bd4
MartinBenfeghoul <63018845+MartinBenfeghoul@users.noreply.github.com>
2026-01-28 13:59:42 +0000

    [feature] add packed token dataloader

--- save_outputs.py
+++ save_outputs.py
@@ -1,9 +1,14 @@
 import torch
+from torch.utils.data import DataLoader
 
-from utils import get_model_and_tokenizer, generate_outputs_single_pass
+from utils import (
+    PackedTokens,
+    load_data,
+    collate,
+    get_model_and_tokenizer, 
+    generate_outputs_single_pass,
+)
 
-
-from torch.nn import CrossEntropyLoss
 
 def calculate_surprise(loss):
     """Calculate surprise from loss."""
@@ -18,23 +23,40 @@ def main(
     model_name,
     dataset,
     save_path="model_outputs.pt",
+    seq_len=2048,
+    micro_bs=8,
 ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model, tokenizer = get_model_and_tokenizer(model_name, device)
 
-    # Example dataset  # TODO: Replace with actual dataset loading
-    ds = [{"prompt": "Hello, how are you?"}, {"prompt": "What is the capital of France?"}]
-
-    inputs, outputs = generate_outputs_single_pass(
-        ds, model, tokenizer, device
+    ds = load_data(dataset)
+    eos_id = tokenizer.eos_token_id
+    packed = PackedTokens(ds, tokenizer, seq_len=seq_len, eos_id=eos_id)
+    dl = DataLoader(
+        packed,
+        batch_size=micro_bs,
+        collate_fn=collate,
+        num_workers=8,
+        pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=4,
     )
+
+    inputs = next(iter(dl))
+    breakpoint()
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+    with torch.no_grad():
+        outputs = model(**inputs, labels=inputs["input_ids"], use_cache=True)
 
     # save outputs to a file
     save_inputs_outputs(inputs, outputs, save_path)
 
 if __name__ == "__main__":
     model_name = "meta-llama/Llama-3.2-1B-Instruct"
-    dataset = "example_dataset"
+    dataset = "HuggingFaceFW/fineweb-edu"  # "example_dataset"
     save_path = "model_outputs.pt"
 
-    main(model_name, dataset, save_path=save_path)
+    main(
+        model_name, dataset, 
+        save_path=save_path
+    )
--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .kv_cache import generate_kv_batched, generate_outputs_single_pass
 from .metrics import eval_model, avg_nll, clean
 from .model import get_model_and_tokenizer
 from .matrix_decomposition import truncated_svd, full_svd
+from .dataloader import PackedTokens, load_data, collate
