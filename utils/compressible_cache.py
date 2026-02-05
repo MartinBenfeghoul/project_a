9259fe86eaf8d5f735e28ea2ae4355088e125181
HHazard2002 <hugo7.hazard@gmail.com>
2026-02-05 12:15:56 +0000

    [feature] added CompressibleCache

--- utils/compressible_cache.py
+++ utils/compressible_cache.py
@@ -118,4 +118,128 @@ class CompressibleLayer(DynamicLayer):
         super().update(max_length)
 
 
+class CompressibleCache(Cache):
+    def __init__(self, 
+                 num_layers: list[int],
+                 hidden_factors: list[int],
+                 device,
+                 memory_func = None,
+                 ddp_cache_data: Iterable[tuple[torch.Tensor | None, ...]] | None = None,
+                 config: PreTrainedConfig | None = None,
+                 offloading: bool = False,
+                 offload_only_non_sliding: bool = False,
+                 ):
+        
+        self.num_layers = num_layers
+        self.device = device
 
+        layers = []
+        # If a config is passed, use it to infer the layer types and initialize accordingly
+        if config is not None:
+            decoder_config = config.get_text_config(decoder=True)
+            sliding_window = getattr(decoder_config, "sliding_window", None) or getattr(
+                decoder_config, "attention_chunk_size", None
+            )
+            layer_types = getattr(decoder_config, "layer_types", None)
+            if layer_types is None:
+                layer_types = [
+                    "sliding_attention" if sliding_window is not None else "full_attention"
+                    for _ in range(decoder_config.num_hidden_layers)
+                ]
+            # Some models have shared layers thus no cache is needed for them (e.g. Gemma3n)
+            if hasattr(decoder_config, "num_kv_shared_layers"):
+                layer_types = layer_types[: -decoder_config.num_kv_shared_layers]
+
+            for layer_type, num_layer, hidden_factor in zip(layer_types, num_layers, hidden_factors):
+                    layers.append(CompressibleLayer(memory_func=memory_func, mem_num_layers=num_layer, mem_hidden_factor=hidden_factor))
+
+        # In this case, use the passed data to already fill in the Cache
+        if ddp_cache_data is not None:
+            # Init all the layers with the data
+            assert (len(ddp_cache_data)== len(num_layers) == len(hidden_factors))
+            for layer_idx, (kv_and_optional_sliding, num_layer, hidden_factor) in enumerate(zip(ddp_cache_data, num_layers, hidden_factors)):
+                # If the config was not passed above, initialize a new cache layer for each entry of the ddp_data
+                if config is None:
+                    # kv_and_optional_sliding contains at least two elements: the key and value states. It can also
+                    # contain a third element, which is an optional sliding window tensor.
+                    layers.append(CompressibleLayer(memory_func=memory_func, mem_num_layers=num_layer, mem_hidden_factor=hidden_factor))
+                # Update the layer with the data
+                _, _ = layers[layer_idx].update(kv_and_optional_sliding[0], kv_and_optional_sliding[1])
+
+        # If neither of config nor ddp_data was passed, then simply lazy init a full cache of DynamicLayer
+        if len(layers) == 0:
+            super().__init__(
+                layer_class_to_replicate=CompressibleLayer,
+                offloading=offloading,
+                offload_only_non_sliding=offload_only_non_sliding,
+            )
+        else:
+            super().__init__(layers=layers, offloading=offloading, offload_only_non_sliding=offload_only_non_sliding)
+
+
+    def eval_memory(
+            self,
+            loss_func = F.mse_loss
+    ) -> None:
+        # Call to evict a percentage or any token under a threshold
+        # This will probably be per model before moving to per layer?
+        all_errors = []
+        all_preds = []
+
+        for layer in self.layers:
+            errors, v_approx = layer.eval_layer(loss_func=loss_func)        
+            all_errors.append(errors.flatten()) 
+            all_preds.append(v_approx) # Keep shape [1, h, t, d]
+
+        return all_errors, all_preds
+
+    def train(
+              self, 
+              num_epochs, 
+              optimizer=Adam, 
+              loss_func=mse_loss, 
+              lr=1.e-3,
+            ):
+        
+        all_params = [p for layer in self.layers for p in layer.memory.parameters()]
+        optimizer = optimizer(all_params, lr=lr)
+        for epoch in range(num_epochs):
+                optimizer.zero_grad()
+                total_loss = 0
+                
+                for layer in self.layers:
+                    keys = layer.keys
+                    values = layer.values
+                    # keys/values shape: [1, num_head, num_token, head_dim]                    
+                    v_hat = layer.memory(keys)
+                    loss = loss_func(v_hat, values)
+                    loss.backward()
+                    total_loss += loss.item()
+                
+                optimizer.step()
+
+                print(f'Epoch {epoch} loss is {total_loss}')
+
+    def compress(
+            self,
+            thresh,
+    ) -> None:
+        # runs train and then evict
+        for layer in self.layers:
+            layer.compress(thresh)
+
+    def replace(
+            self,
+            threshold_value,
+    ) -> None:
+        count = 0
+        for layer in self.layers:
+            count += layer.replace(threshold_value)
+        return count
+    
+    def decompress(
+            self,
+    ) -> None:  
+        for layer in self.layers:
+            layer.decompress()
+        
