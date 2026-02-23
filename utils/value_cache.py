d33ea33b1c670c71c10911be26357d5a0f57a245
HHazard2002 <hugo7.hazard@gmail.com>
2026-02-23 17:28:31 +0100

    [feature] added the crop function to MLPValueLayer

--- utils/value_cache.py
+++ utils/value_cache.py
@@ -5,6 +5,7 @@ from torch.nn.functional import mse_loss
 import torch
 from model.mlp import MLP
 from transformers.cache_utils import Any
+import numpy as np
 
 LOSS_FUNC = {
     'mse': mse_loss
@@ -43,6 +44,7 @@ class MLPValueLayer(SingleTensorDynamicLayer):
         self.compressed_values = None
         self.is_compressed = False
         self.prefill = True
+        self.compressed_len = 0
         
     def lazy_initialization(
             self,
@@ -74,8 +76,8 @@ class MLPValueLayer(SingleTensorDynamicLayer):
     
     def train_mlp(self, keys):
         with torch.enable_grad():
-            keys = keys
-            values = self.tensor
+            values = self.tensor.detach()
+            keys = keys.detach()
             all_params = [p for p in self.mlp.parameters()]
             optimizer = self.optimizer_cls(all_params, lr=self.lr)
             for _ in range(self.num_epochs):
@@ -86,7 +88,7 @@ class MLPValueLayer(SingleTensorDynamicLayer):
                 loss.backward()                
                 optimizer.step()
 
-    def compress(self, threshold_val, keys) -> int:
+    def compress(self, threshold_val, keys):
         v_approx = self.mlp(keys)
         errors = self.loss_func(self.tensor, v_approx, reduction='none').mean(dim=-1)
         mask = errors > threshold_val
@@ -96,11 +98,12 @@ class MLPValueLayer(SingleTensorDynamicLayer):
         #print(f"Number of values kept (error > {threshold_val}): {num_kept}")
         self.compressed_values = self.tensor[b, h, t]
         #print(self.compressed_values.numel())
+        self.compressed_len = self.tensor.shape[2]
         self.tensor = torch.tensor([], dtype=keys.dtype, device=self.tensor.device)
         self.is_compressed = True
-
+        
     def temp_decompress(self, keys) -> torch.Tensor:
-        values = self.mlp(keys)
+        values = self.mlp(keys[:, :, :self.compressed_len, :])
         b, h, t = self.indices
         values[b, h, t] = self.compressed_values
         return values
@@ -125,41 +128,48 @@ class MLPValueLayer(SingleTensorDynamicLayer):
 
         values = super().update(value_states)
 
-        #is_prefill = self.compressed_values.numel() == 0
-
-        print(f'Keys: {keys.shape}')
-        print(f'Values: {values.shape}')
+        #print(f'Keys: {keys.shape}')
+        #print(f'Values: {values.shape}')
         
-
-        if self.is_prefill:
+        if self.prefill:
             #print(self.compressed_values.numel())
             #print("We are in prefill")
             self.train_mlp(keys)
             self.compress(threshold_val=0.001, keys=keys)
-            self.is_prefill = False
-            print(f'Indices {self.indices[0].shape}')
-            print(f'Compressed values {self.compressed_values.shape}')
+            self.prefill = False
+            #print(f'Indices {self.indices[0].shape}')
+            #print(f'Compressed values {self.compressed_values.shape}')
             return values
         elif self.is_compressed:
             #print("We are generating")
             decomp_values = self.temp_decompress(keys)
-            decomp_values = torch.cat([decomp_values[..., :-self.tensor.shape[-2], :], self.tensor], dim=-2)
-            print(f'Decompressed values {decomp_values.shape}')
+            decomp_values = torch.cat([decomp_values, self.tensor], dim=-2)
+            #print(f'Decompressed values {decomp_values.shape}')
             return decomp_values
         else:
             raise Exception(
                 "Prefill is set to False but the values where not compressed."
             ) 
 
-    # NEED TO FIX CROP POTENTIALLY NEED TO 1. REGENERATE FULL KEY, 
-    # 2. CROP KEY, 3. PASS FULL KEY (NOT CROPED) TO VALUE CACHE, 
-    # 4. DECOMPRESS AND CROP VALUE
     def crop(self, max_length: int) -> None:
-        if self.is_compressed == True:
-            self.decompress()
+        if self.compressed_len < max_length: 
+            # If the cache isn't compressed or max_length is larger just crop the suffix tensor
+            new_max_length = max_length - self.compressed_len
+            super().crop(new_max_length)
+            return
         
-        super().crop(max_length)
+        # Otherwise we generate a mask of the idx to keep and update the different tensors
+        self.compressed_len = max_length
+        self.tensor = torch.tensor([], dtype=self.tensor.dtype, device=self.tensor.device)
+        b, h, t = self.indices
 
+        if self.indices[0].numel() == 0:
+            return
+    
+        keep_idx = t < max_length
+
+        self.indices = (b[keep_idx], h[keep_idx], t[keep_idx])
+        self.compressed_values = self.compressed_values[keep_idx]
 
 class MLPValueCache(SingleTensorCache):
     def __init__(
