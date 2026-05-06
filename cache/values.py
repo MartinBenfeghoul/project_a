[Update] change default MLP parameters to best and minor bug fix in compression ratio calculation

cache/values.py
@@ -432,7 +432,6 @@ class MLPValueCache(SingleTensorCache):
         hidden_factors_per_mlp: list[int],
         num_heads_per_mlp: list[int],
         target_perc: list[float],
-        target_model_num_heads: int = 8,
         per_sequence: bool = False,
         lr: float = 1e-3,
         optimizer: str = "adam",
@@ -470,7 +469,6 @@ class MLPValueCache(SingleTensorCache):
         self.target_perc = target_perc
         self.per_sequence = per_sequence
 
-        self.target_model_num_heads = target_model_num_heads
 
         self.lr = lr
 
@@ -618,7 +616,7 @@ class MLPValueCache(SingleTensorCache):
         compressed_total = 0
 
         for layer in self.layers:
-            h, d = self.target_model_num_heads, layer.head_dim
+            h, d = layer.num_heads, layer.head_dim
             t = layer.compressed_len
             delta_dtype = layer.tensor.dtype
             value_dtype_size = torch.finfo(layer.tensor.dtype).bits / 8

lm_eval_script.py
@@ -210,7 +210,6 @@ def main(args):
         "num_heads_per_mlp": [args.num_heads_per_mlp] * num_layers,
         "per_sequence": args.per_sequence,
         "target_perc": target_perc_per_layer,
-        "target_model_num_heads": args.target_model_num_heads,
         "lr": args.v_lr,
         "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         "optimizer": args.optimizer,
@@ -435,13 +434,12 @@ def parse_args():
 
     # value cache
     parser.add_argument("-vc", "--v_cache_type", type=str, default="mlp")
-    parser.add_argument("--num_layers_per_mlp", type=int, default=4)
-    parser.add_argument("--hidden_factors_per_mlp", type=int, default=2)
+    parser.add_argument("--num_layers_per_mlp", type=int, default=2)
+    parser.add_argument("--hidden_factors_per_mlp", type=int, default=1)
     parser.add_argument("--num_heads_per_mlp", type=int, default=8)
     parser.add_argument("--per_sequence", action="store_true")
     parser.add_argument("--target_perc", type=int, default=85)
     parser.add_argument("--target_cr", type=float, default=None)
-    parser.add_argument("--target_model_num_heads", type=int, default=8)
     parser.add_argument("--v_lr", type=float, default=1e-3)
     parser.add_argument(
         "--optimizer",